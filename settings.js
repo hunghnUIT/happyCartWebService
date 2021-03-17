accf66c1e5f1e0a73ea2fd7316b6167a500eb40f
hunghnuit <hunghn0999@gmail.com>
2021-03-17 16:42:29 +0700

    Not many thing changed

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ const HEADERS_SHOPEE = {
     'Connection': 'keep-alive',
     'if-none-match-': '55b03-14bc0d8585b6a1c6ef3f05c0c3078db0', 
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
-    // 'user-agent': 'PostmanRuntime/7.26.8',
+    // 'User-Agent': 'Mozilla/5.0 (Compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
     'Referer':' https://shopee.vn/',
     'Accept-Encoding':' gzip, deflate, br',
     'Accept-Language':' en-US,en;q=0.9',
@@ -12,6 +12,7 @@ const HEADERS_SHOPEE = {
 const HEADERS_TIKI = {
     'Connection': 'keep-alive',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+    // 'User-Agent': 'Mozilla/5.0 (Compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
     'Accept': '*/*',
     'Sec-Fetch-Site':' cross-site',
     'Sec-Fetch-Mode':' cors',
