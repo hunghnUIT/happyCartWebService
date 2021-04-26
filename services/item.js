dea183f80e09ec76aa33f5d3bf0e547acf5fae07
hunghnuit <hunghn0999@gmail.com>
2021-04-26 16:40:19 +0700

    Change name of item redis listToCrawl

--- services/item.js
+++ services/item.js
@@ -29,5 +29,5 @@ exports.addItemToCrawlingList = async (itemId, sellerId, platform) => {
         itemId: itemId,
         sellerId: sellerId,
     };
-    await redis.hset(`listItemToCrawl-${platform}`, `${itemId}-${platform}`, JSON.stringify(value)).catch(error => console.error(error));
+    await redis.hset(`listItemToCrawl-${platform}`, `${itemId}`, JSON.stringify(value)).catch(error => console.error(error));
 };
